c51354cdbe005e46eba02e6365864c4d811bd15c
Mohamed Monir shabana <mohamed_ms2006@hotmail.com>
2018-10-18 11:19:50 +0200

    change Login Roles for password Length

---
+++ CustomeSite/CustomeSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CustomeSite.Startup))]
+namespace CustomeSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
